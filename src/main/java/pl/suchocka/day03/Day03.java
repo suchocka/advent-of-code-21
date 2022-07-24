24d623abf330d9bd425943fa390a1010893e462b
Klaudia Suchocka <klaudia.suchocka139@gmail.com>
2022-07-24 17:52:31 +0200

    solved day 3 - part 1 - CR

--- src/main/java/pl/suchocka/day03/Day03.java
+++ src/main/java/pl/suchocka/day03/Day03.java
@@ -14,11 +14,11 @@ public class Day03 {
     public int run(String fileName) throws IOException {
         List<String> inputs = readInput(fileName);
         Report report = generateReport(inputs);
-        return getPowerConsumption(report);
+        return report.getPowerConsumption();
     }
 
     private Report generateReport(List<String> inputs) {
-        int[][] matrix = mapInputsToMatrix(inputs);
+        Integer[][] matrix = mapInputsToMatrix(inputs);
         StringBuilder gammaRate = new StringBuilder();
         StringBuilder epsilonRate = new StringBuilder();
         for (int col = 0; col < inputs.get(0).length(); col++) {
@@ -31,40 +31,24 @@ public class Day03 {
                     oneCounter++;
                 }
             }
-            if (oneCounter > zeroCounter) {
-                gammaRate.append(1);
-                epsilonRate.append(0);
-            } else {
-                gammaRate.append(0);
-                epsilonRate.append(1);
-            }
-            oneCounter = 0;
-            zeroCounter = 0;
+            boolean oneDominates = oneCounter > zeroCounter;
+            gammaRate.append(oneDominates ? 1 : 0);
+            epsilonRate.append(oneDominates ? 0 : 1);
         }
         return new Report(binaryToDecimal(gammaRate.toString()), binaryToDecimal(epsilonRate.toString()));
     }
 
-    private int[][] mapInputsToMatrix(List<String> inputs) {
-        int[][] matrix = new int[inputs.size()][inputs.get(0).length()];
-        for (int row = 0; row < inputs.size(); row++) {
-            String input = inputs.get(row);
-            String[] numbers = input.split("");
-            for (int col = 0; col < input.length(); col++) {
-                matrix[row][col] = Integer.parseInt(numbers[col]);
-            }
-        }
-        return matrix;
+    private Integer[][] mapInputsToMatrix(List<String> inputs) {
+        return inputs.stream()
+                .map(line -> line.chars().mapToObj(x -> x - '0').toArray(Integer[]::new))
+                .toArray(Integer[][]::new);
     }
 
-    private int getPowerConsumption(Report report) {
-        return report.getGammaRate() * report.getEpsilonRate();
-    }
-
-    private int binaryToDecimal(String binary) {
+    private static int binaryToDecimal(String binary) {
         return Integer.parseInt(binary, 2);
     }
 
-    private List<String> readInput(String fileName) throws IOException {
+    private static List<String> readInput(String fileName) throws IOException {
         return Files.readAllLines(Path.of(fileName));
     }
 
@@ -74,5 +58,9 @@ public class Day03 {
     static class Report {
         private Integer gammaRate;
         private Integer epsilonRate;
+
+        public int getPowerConsumption() {
+            return getGammaRate() * getEpsilonRate();
+        }
     }
 }
